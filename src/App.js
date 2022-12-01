f9f99dd7e9cafa9a83a3a1ae335e6b08a9b1b7f0
Anastasiia Ponkratova <s20488@pjwstk.edu.pl>
2022-12-01 14:44:58 +0200

    37. store, state, ОПП, рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./components/Header/Header";
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar state={props.state.dialogsPage.sidebar}/>
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        {/*Для React v6: вместо exact ставим *  для дочерних URL-адресов*/}
+                        <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage}/>}/>
+                        <Route path="/profile/*" element={<Profile profilePage={props.state.profilePage}
+                                                                   updateNewPostText={props.updateNewPostText}
+                                                                   addPost={props.addPost}/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
