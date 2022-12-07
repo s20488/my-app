c25fa5fe4bdc269a48974f2487b8208a2f28556c
Anastasiia Ponkratova <s20488@pjwstk.edu.pl>
2022-12-07 16:29:22 +0200

    action creator, action type

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from "react";
-import Header from "./components/Header/Header";
+import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar/Navbar";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import Profile from "./components/Profile/Profile";
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 
-function App(props) {
+const App = (props) => {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar state={props.state.dialogsPage.sidebar}/>
-                <div class='app-wrapper-content'>
-                    <Routes>
-                        {/*Для React v6: вместо exact ставим *  для дочерних URL-адресов*/}
-                        <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage}/>}/>
-                        <Route path="/profile/*" element={<Profile profilePage={props.state.profilePage}
-                                                                   updateNewPostText={props.updateNewPostText}
-                                                                   addPost={props.addPost}/>}/>
-                    </Routes>
-                </div>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <div className='app-wrapper-content'>
+                <Route path='/dialogs'
+                       render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
+
+                <Route path='/profile'
+                       render={ () => <Profile
+                           profilePage={props.state.profilePage}
+                           dispatch={props.dispatch} /> }/>
             </div>
-        </BrowserRouter>
-    );
+        </div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import './index.css';
+import * as serviceWorker from './serviceWorker';
 import store from "./redux/state";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
-    root.render(
-        <React.StrictMode>
-            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
-        </React.StrictMode>);
-};
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} dispatch={store.dispatch.bind(store)} />
+        </BrowserRouter>, document.getElementById('root'));
+}
 
 rerenderEntireTree(store.getState());
 
 store.subscribe(rerenderEntireTree);
+
+
+// API
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
