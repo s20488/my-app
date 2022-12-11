1963ce68b59471328c3a09fe90e88d66b7065870
Anastasiia Ponkratova <serlee001@gmail.com>
2022-12-11 12:55:03 +0200

    ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,21 +7,12 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>, document.getElementById('root'));
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe( () => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-})
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
 
 
 // API
