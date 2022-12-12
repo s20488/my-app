b3b74b40aa6a56d255493bcc3763e728d0418d9c
Anastasiia Ponkratova <serlee001@gmail.com>
2022-12-12 17:35:09 +0200

    Users API, install axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import s from './users.module.css'
+import avatarUsers from './../../avatar.jpg';
+import axios from "axios";
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    avatar: 'https://avatars.mds.yandex.net/i?id=3371c45886ba7b9626277725061c98c9_l-5644585-images-thumbs&n=27&h=384&w=480',
-                    fullName: 'Dimych',
-                    status: 'I am free',
-                    location: {city: 'Moskow', country: 'Russia'},
-                    followed: true
-                },
-                {
-                    id: 2,
-                    avatar: 'https://avatars.mds.yandex.net/i?id=3371c45886ba7b9626277725061c98c9_l-5644585-images-thumbs&n=27&h=384&w=480',
-                    fullName: 'Katy',
-                    status: 'I am free',
-                    location: {city: 'Moskow', country: 'Russia'},
-                    followed: false
-                },
-                {
-                    id: 3,
-                    avatar: 'https://avatars.mds.yandex.net/i?id=3371c45886ba7b9626277725061c98c9_l-5644585-images-thumbs&n=27&h=384&w=480',
-                    fullName: 'Alice',
-                    status: 'I am free',
-                    location: {city: 'Moskow', country: 'Russia'},
-                    followed: true
-                }
-            ]
-        );
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     return <div>
@@ -38,7 +16,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.avatar} className={s.usersAvatar}/>
+                        <img src={u.photos.small != null ? u.photos.small : avatarUsers} className={s.usersAvatar}/>
                     </div>
                     <div>
                        {u.followed
@@ -52,12 +30,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
