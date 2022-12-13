74a3caec376472912475ce9f6272feb763b197c6
Anastasiia Ponkratova <serlee001@gmail.com>
2022-12-13 11:05:29 +0200

    componentDidMount (life cycle methods)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,18 +5,15 @@ import axios from "axios";
 
 class Users extends React.Component {
 
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                this.props.setUsers(response.data.items);
-            });
-        }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
 
 
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get users</button>
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>
