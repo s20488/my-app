1190a4461c53df85794f5e89c91f74455488fbf2
Anastasiia Ponkratova <serlee001@gmail.com>
2022-12-13 10:30:05 +0200

    class component (extends React.Component)

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import s from './users.module.css'
+import avatarUsers from './../../avatar.jpg';
+import axios from "axios";
+
+class Users extends React.Component {
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers(response.data.items);
+            });
+        }
+    }
+
+
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : avatarUsers} className={s.usersAvatar}/>
+                    </div>
+                    <div>
+                       {u.followed
+                           ? <button onClick={() => {
+                               this.props.unfollow(u.id)
+                           }}>Unfollow</button>
+                           : <button onClick={() => {
+                               this.props.follow(u.id)
+                           }}>Follow</button>}
+                    </div>
+                </span>
+                    <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+                </div>)
+            }
+        </div>
+    }
+}
+
+export default Users;
