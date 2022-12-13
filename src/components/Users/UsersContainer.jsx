5ba413469d60e93561e9a6eb64f722c80d0fb5d7
Anastasiia Ponkratova <serlee001@gmail.com>
2022-12-13 20:37:15 +0200

    pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../../redux/users-reducer";
+import {
+    followActionCreator,
+    setCurrentPageActionCreator,
+    setTotalUsersCountActionCreator,
+    setUsersActionCreator,
+    unfollowActionCreator
+} from "../../redux/users-reducer";
 import Users from "./Users";
 
 const mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -19,6 +28,12 @@ const mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersActionCreator(users));
+        },
+        setCurrentPage: (currentPage) => {
+            dispatch(setCurrentPageActionCreator(currentPage));
+        },
+        setTotalUsersCount: (totalUsersCount) => {
+            dispatch(setTotalUsersCountActionCreator(totalUsersCount))
         }
     }
 }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 2
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -13,7 +18,7 @@ const usersReducer = (state = initialState, action) => {
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if(u.id === action.userID) {
+                    if (u.id === action.userID) {
                         return {...u, followed: true};
                     }
                     return u;
@@ -24,7 +29,7 @@ const usersReducer = (state = initialState, action) => {
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if(u.id === action.userID) {
+                    if (u.id === action.userID) {
                         return {...u, followed: false};
                     }
                     return u;
@@ -32,9 +37,17 @@ const usersReducer = (state = initialState, action) => {
             };
 
         case SET_USERS: {
-            return {...state, users: [...state.users, ...action.users]
-            };
-        }
+            return {...state, users: action.users}
+        };
+
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        };
+
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.totalUsersCount}
+        };
+
         default:
             return state;
     };
@@ -43,5 +56,7 @@ const usersReducer = (state = initialState, action) => {
 export const followActionCreator = (userID) => ({type: FOLLOW, userID});
 export const unfollowActionCreator = (userID) => ({type: UNFOLLOW, userID});
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users});
+export const setCurrentPageActionCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountActionCreator = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount});
 
 export default usersReducer;
